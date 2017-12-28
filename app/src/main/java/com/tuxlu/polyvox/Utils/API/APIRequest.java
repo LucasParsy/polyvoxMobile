bd8eab6f7e04e1ecc5f2cbf3d13cb02eb42c8c97
parsy_a <lucas.parsy@epitech.eu>
2017-12-28 09:24:02 +0100

    refactored APIJsonRequest
    the method is now more modifiable, and may prevent leaks.
    created MultiPartequest method, the class coming in next commit
    
    intermediate commit - does not compile

--- app/src/main/java/com/tuxlu/polyvox/Utils/API/APIRequest.java
+++ app/src/main/java/com/tuxlu/polyvox/Utils/API/APIRequest.java
@@ -20,16 +20,15 @@ import com.tuxlu.polyvox.R;
 import com.tuxlu.polyvox.User.Login;
 import com.tuxlu.polyvox.Utils.Auth.AuthUtils;
 import com.tuxlu.polyvox.Utils.NetworkLibraries.VHttp;
+import com.tuxlu.polyvox.Utils.NetworkLibraries.VolleyMultipartRequest;
 import com.tuxlu.polyvox.Utils.NetworkUtils;
-import com.tuxlu.polyvox.Utils.ToastType;
 import com.tuxlu.polyvox.Utils.UtilsTemp;
 
 import org.json.JSONObject;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-
-import butterknife.internal.Utils;
 
 /**
  * Created by tuxlu on 19/12/17.
@@ -37,93 +36,126 @@ import butterknife.internal.Utils;
 
 public class APIRequest {
 
-
     public static boolean isAPIConnected(Context context) {
         AccountManager am = AccountManager.get(context);
         Account[] accounts = am.getAccounts();
         return (accounts.length != 0);
     }
 
+    public static boolean checkConnection(Context context, boolean usesApi) {
+        if (!NetworkUtils.isConnected(context)) {
+            UtilsTemp.showToast(context, context.getString(R.string.no_network));
+            return false;
+        }
+
+        if (usesApi && !isAPIConnected(context)) {
+            startLoginActivity(context);
+            return false;
+        }
+        return true;
+    }
+
     public static void JSONrequest(final Context context, final int method, final String url,
                                    final boolean usesApi, final JSONObject body,
                                    final Response.Listener<JSONObject> listener,
                                    final Response.ErrorListener errorListener) {
-        if (!NetworkUtils.isConnected(context))
-        {
-            UtilsTemp.showToast(context, context.getString(R.string.no_network));
+        if (!checkConnection(context, usesApi))
             return;
-        }
-
-        AsyncTask.execute(new Runnable() {
-            @Override
-            public void run() {
-                if (usesApi && !isAPIConnected(context)) {
-                    startLoginActivity(context);
-                    return;
-                }
+        AsyncTask.execute(new Runnable() { @Override public void run() {
                 final String token = usesApi ? AuthUtils.getToken(context) : null;
-                JsonObjectRequest jsObjRequest = new JsonObjectRequest
-                        (method, url, body, listener, new Response.ErrorListener() {
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                if (error.networkResponse != null) {
-                                    if (error.networkResponse.statusCode == APIUrl.TOKEN_DENIED_CODE) {
-                                        AuthUtils.removeAccountLogout(context);
-                                        startLoginActivity(context);
-                                        UtilsTemp.showToast(context, context.getString(R.string.logout_been));
-                                        return;
-                                    }
-                                    if (error.networkResponse.statusCode == APIUrl.USER_NOT_VALIDATED) {
-                                        AlertDialog.Builder build = new AlertDialog.Builder(context);
-                                        build.setTitle(context.getString(R.string.not_verified_title))
-                                                .setMessage(context.getString(R.string.not_verified_message))
-                                                .setPositiveButton(context.getString(R.string.not_verified_resend), new DialogInterface.OnClickListener() {
-                                                    public void onClick(DialogInterface dialog, int id) {
-                                                        NetworkUtils.sendMail(context);
-                                                    }
-                                                })
-                                                .setNegativeButton(context.getString(R.string.not_verified_nope), null)
-                                                .show();
-                                    }
-                                }
-                                NetworkUtils.checkNetworkError(context, error);
-
-                                if (BuildConfig.DEBUG) {
-                                    error.printStackTrace();
-                                    if (error.networkResponse != null) {
-                                        NetworkResponse resp = error.networkResponse;
-                                        String data = new String(resp.data);
-                                        Log.wtf("NETWORK", "code = " + resp.statusCode +
-                                                "\n data=" + data);
-                                    }
-                                }
-
-                                if (errorListener != null)
-                                    errorListener.onErrorResponse(error);
-                            }
-                        }) {
-                    @Override
-                    public Map<String, String> getHeaders() throws AuthFailureError {
-                        if (!usesApi)
-                            return super.getHeaders();
-                        Map<String, String> params = new HashMap<>();
-                        params.put(APIUrl.COOKIE_HEADER, token);
-                        return params;
-                    }
-                };
-
-                //avoid timeout
-                jsObjRequest.setRetryPolicy(new DefaultRetryPolicy(
-                        15000,
-                        DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
-                        DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
-
-                VHttp.getInstance(context.getApplicationContext()).addToRequestQueue(jsObjRequest);
+                APIJsonObjectRequest jsObjRequest = new APIJsonObjectRequest(method, url, body, listener, errorListener, token, context);
+                addRequestToQueue(context, usesApi, jsObjRequest);
             }
         });
+    }
+
+
+    public static void Multipartequest(final Context context, final int method, final String url,
+                                       final boolean usesApi, final List<VolleyMultipartRequest.DataPart> body,
+                                       final Response.Listener<JSONObject> listener,
+                                       final Response.ErrorListener errorListener) {
+
+        if (!checkConnection(context, usesApi))
+            return;
+        AsyncTask.execute(new Runnable() { @Override public void run() {
+                final String token = usesApi ? AuthUtils.getToken(context) : null;
+                VolleyMultipartRequest request = new VolleyMultipartRequest(method, url, body, listener, errorListener, token, context);
+                addRequestToQueue(context, usesApi, request);
+            }
+        });
+    }
+
+
+    public static void addRequestToQueue(final Context context, final boolean usesApi, final APIJsonObjectRequest request) {
+        //avoid timeout
+        request.setRetryPolicy(new DefaultRetryPolicy(
+                15000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        VHttp.getInstance(context.getApplicationContext()).addToRequestQueue(request);
     }
 
     public static void startLoginActivity(Context context) {
         context.startActivity(new Intent(context, Login.class));
     }
+
+
+    static public class APIJsonObjectRequest extends JsonObjectRequest {
+        private String token;
+
+        public APIJsonObjectRequest(int method, String url, JSONObject jsonRequest,
+                                    Response.Listener<JSONObject> listener, final Response.ErrorListener errorListener, String nToken, final Context context) {
+
+            super(method, url, jsonRequest, listener, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    if (error.networkResponse != null) {
+                        if (error.networkResponse.statusCode == APIUrl.TOKEN_DENIED_CODE) {
+                            AuthUtils.removeAccountLogout(context);
+                            startLoginActivity(context);
+                            UtilsTemp.showToast(context, context.getString(R.string.logout_been));
+                            return;
+                        }
+                        if (error.networkResponse.statusCode == APIUrl.USER_NOT_VALIDATED) {
+                            AlertDialog.Builder build = new AlertDialog.Builder(context);
+                            build.setTitle(context.getString(R.string.not_verified_title))
+                                    .setMessage(context.getString(R.string.not_verified_message))
+                                    .setPositiveButton(context.getString(R.string.not_verified_resend), new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int id) {
+                                            NetworkUtils.sendMail(context);
+                                        }
+                                    })
+                                    .setNegativeButton(context.getString(R.string.not_verified_nope), null)
+                                    .show();
+                        }
+                    }
+                    NetworkUtils.checkNetworkError(context, error);
+
+                    if (BuildConfig.DEBUG) {
+                        error.printStackTrace();
+                        if (error.networkResponse != null) {
+                            NetworkResponse resp = error.networkResponse;
+                            String data = new String(resp.data);
+                            Log.wtf("NETWORK", "code = " + resp.statusCode +
+                                    "\n data=" + data);
+                        }
+                    }
+
+                    if (errorListener != null)
+                        errorListener.onErrorResponse(error);
+                }
+            });
+            token = nToken;
+        }
+
+        @Override
+        public Map<String, String> getHeaders() throws AuthFailureError {
+            if (token == null)
+                return super.getHeaders();
+            Map<String, String> params = new HashMap<>();
+            params.put(APIUrl.COOKIE_HEADER, token);
+            return params;
+        }
+    }
+
 }
